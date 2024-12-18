3fa8353838a99e9e124086effb0f5b524b1b9d4e
Glor1ous <pushin.illya@yandex.ru>
2024-12-19 00:25:53 +0300

    first commit

---
+++ myproject/users/admin.py
@@ -0,0 +1,5 @@
+
+from django.contrib import admin
+# from django.contrib.auth.models import User  # Удалите, если не нужно
+
+# Удалите или закомментируйте admin.site.register(User), так как модель уже зарегистрирована
---
+++ myproject/users/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def home(request):
+    return render(request, 'home.html')
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
